2e95cfd410b92927fad92e811cba344d78585cfe
Matias Bruna <matibruna6@gmail.com>
2023-02-21 00:35:09 -0300

    Clase 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container } from "@mui/material";
+import { Searcher } from "./components/Searcher";
 
-export default App = () => {
+export default function App(){
+
+    const [user, setUser] = useState("octocat");
+    const [userStater, userState] = useState(user);
+
     return(
-        <div>Soy un estudiante de platzi!</div>
+            <Container sx={{
+                background: "linear-gradient(90deg, rgba(33,0,36,0.88) 17%, rgba(24,4,69, 0.87) 38%, rgba(9,9,121,0.85) 60%, rgba(0,112,255,1) 100%)",
+                width: "80vw",
+                height: "500px",
+                borderRadius: "15px",
+                marginTop: "50px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}>
+                <Searcher setUser={setUser} />
+            </Container>
     )
 }
---
+++ src/components/Searcher/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { IconButton, Stack, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+export const Searcher = (props) => {
+
+    const {
+        setUser
+    } = props;
+
+    const [value, setValue] = useState("");
+
+    const onSearch = (e) => {
+        setUser(value);
+    }
+
+    return(
+    <Stack direction="row" sx={{
+        marginTop: "50px",
+        width: "75%",
+    }}>
+        <TextField 
+            value={value}
+            onChange={event=>setValue(event.target.value)}
+            label="GitHub User"
+            placeholder="Octocat"
+            size="small"
+            sx={{
+                width: "100%",
+                input: {
+                    color: "white"
+                },
+                '& label.Mui-focused': {
+                    color: 'white',
+                },
+                '& label': {
+                    color: 'white',
+                },
+                '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                        borderColor: 'white',
+                    },
+                    '&:hover fieldset': {
+                        borderColor: 'white',
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderColor: 'white'
+                    }
+                }
+            }}/>
+        <IconButton 
+            onClick={onSearch}
+            size="small"
+            sx={{
+                color: "white",
+                left: "-40px",
+        }}>
+            <SearchIcon />
+        </IconButton>
+    </Stack>);
+}
