76722389e9ba948e4e55910ce3e1c03ccac79891
Matias Bruna <matibruna6@gmail.com>
2023-04-18 15:36:55 -0300

    Build change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Searcher } from "./components/Searcher";
 import { getGitHubUser } from "./services/getGitHubUser";
 import UserCard from "./containers/userCard";
 
-export default function App(){
+export default const App = () => {
 
     const [user, setUser] = useState("octocat");
     const [userState, setUserState] = useState('inputUser');
